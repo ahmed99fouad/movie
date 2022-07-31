ef2b9ff29de546372c9a5bdbc12f419ddf3af458
ahmedfouad <53119627+ahmed99fouad@users.noreply.github.com>
2022-07-31 19:43:53 +0200

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,37 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import MovieDetail from "../components/MovieDetail.vue";
+import Actors from "../components/Actors.vue";
+import ActorDetail from "../components/ActorDetail.vue";
+Vue.use(VueRouter);
 
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+const routes = [{
+        path: "/",
+        name: "HomeView",
+        component: HomeView,
+    },
+    {
+        path: "/movie/:id",
+        name: "Home",
+        component: MovieDetail,
+    },
+    {
+        path: "/actors",
+        name: "actors",
+        component: Actors,
+    },
+    {
+        path: "/actor/:id",
+        name: "actor",
+        component: ActorDetail,
+    },
+];
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
-})
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes,
+});
 
-export default router
+export default router;
